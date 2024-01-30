fe57ce25aa0426dd77c5607ae66d0731740cea70
Sumit <kumarxumit6@gmail.com>
2024-01-31 02:56:43 +0530

    added folder structure
    Signed-off-by: Sumit <kumarxumit6@gmail.com>

---
+++ arrays/Easy/qus1.java
@@ -0,0 +1,24 @@
+package Easy;
+
+// Given an array of size N-1 such that it only contains distinct integers in the range of 1 to N. Find the missing element.
+
+// Example 1:
+
+// Input:
+// N = 5
+// A[] = {1,2,3,5}
+// Output: 4
+
+
+
+
+public class qus1 {
+    int missingNumber(int array[], int n) {
+        int sumArray =0;
+        for(int i=0 ; i < array.length;i++){
+            sumArray+=array[i];
+        }
+        int totalSum = n*(n+1) / 2;
+        return totalSum - sumArray;
+     }
+}
