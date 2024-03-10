9ab4be6298df4198b44d2b4c94b9eaf6cdd026c6
Sumit <kumarxumit6@gmail.com>
2024-03-10 21:29:57 +0530

    qus4 solved gfg
    Signed-off-by: Sumit <kumarxumit6@gmail.com>

---
+++ string/Basic/qus4.java
@@ -0,0 +1,17 @@
+// Given a string s. The task is to convert characters of string to lowercase.
+
+// Example 1:
+
+// Input: 
+// s = "ABCddE"
+// Output: "abcdde"
+// Explanation: A, B, C and E are converted to a, b, c and E thus all uppercase characters of the string converted to lowercase letter.
+
+
+
+public class qus4 {
+    static String toLower(String s) {
+        
+        return s.toLowerCase();
+    }
+}
