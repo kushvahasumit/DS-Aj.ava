001d55a6e09275af01dd6b460c98a8a758c93955
Sumit <kumarxumit6@gmail.com>
2024-04-13 00:58:16 +0530

    refactor dublicate remove code
    Signed-off-by: Sumit <kumarxumit6@gmail.com>

--- arrays/practice/qus11.java
+++ arrays/practice/qus11.java
@@ -1,7 +1,7 @@
 public class qus11 {
     public static int dublicate(int arr[]){
         int k = 0;
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 1; i < arr.length; i++) {
             if (arr[k] != arr[i]) {
                 arr[k + 1] = arr[i];
                 k++;
@@ -11,7 +11,7 @@ public class qus11 {
     }
     
     public static void main(String[] args) {
-        int arr[] = {1,2,3,4,3,5,6,6};
+        int arr[] = {1,1,2,2,3,3,4,5,6,6};
         int result = dublicate(arr);
         for (int i = 0; i < result; i++) {
             System.out.println(arr[i]);
