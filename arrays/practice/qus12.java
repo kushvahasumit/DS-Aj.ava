a4cb4f88d5d0cccba2f664afe0a277fb51e4c7da
Sumit <kumarxumit6@gmail.com>
2024-04-13 01:18:30 +0530

    factorial practice
    Signed-off-by: Sumit <kumarxumit6@gmail.com>

---
+++ arrays/practice/qus12.java
@@ -0,0 +1,11 @@
+public class qus12 {
+    public static void main(String[] args) {
+        int m = 5;
+        int factorial = 1;
+        for (int i = 1; i <= m; i++) {
+            factorial *= i;
+        }
+
+        System.out.println(factorial);
+    }
+}
