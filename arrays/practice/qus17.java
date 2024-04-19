dbdf67eac5ff16aa246120ddb2c71c5a8b88c687
Sumit <kumarxumit6@gmail.com>
2024-04-20 00:21:18 +0530

    new qus
    Signed-off-by: Sumit <kumarxumit6@gmail.com>

---
+++ arrays/practice/qus17.java
@@ -0,0 +1,10 @@
+public class qus17 {
+    public static int intersection(int[] arr1,int[] arr2){
+        
+    }
+    public static void main(String[] args) {
+        int[] arr1={2,3,4,5,6,7,9};
+        int[] arr2={5,6,3,2,1,8,9};
+
+    }
+}
