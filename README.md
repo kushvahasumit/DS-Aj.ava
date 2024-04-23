35d6bd9300d35f0b6dfdc9ccd4f9b8831727188f
Sumit <kumarxumit6@gmail.com>
2024-04-24 03:25:22 +0530

    arraylist basic qus

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ ko# DSA Learning Repository
 This repository contains Java solutions for Data Structures and Algorithms problems from GeeksforGeeks.
 
 ## Topics
-- [Arrays]
-- [Strings]
+- [Arrays](arrays)
+- [Strings](string)
 
 Feel free to explore the folders to find solutions for specific topics.
--- arrays/practice/qus17.java
+++ arrays/practice/qus17.java
@@ -1,10 +1,20 @@
+import java.util.ArrayList;
+
 public class qus17 {
-    public static int intersection(int[] arr1,int[] arr2){
-        
-    }
     public static void main(String[] args) {
-        int[] arr1={2,3,4,5,6,7,9};
-        int[] arr2={5,6,3,2,1,8,9};
+
+        ArrayList<Integer> list = new ArrayList<>(6);
+
+        list.add(2);
+        list.add(13);
+        list.add(44);
+        list.add(50);
+        list.add(16);
+        list.add(37);
+
+        for (int a : list) {
+            System.out.print(a);
+        }
 
     }
 }
