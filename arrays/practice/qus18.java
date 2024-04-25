e8f0464b81e78f0e8963cf044ca4992e22749640
Sumit <kumarxumit6@gmail.com>
2024-04-26 00:15:59 +0530

    refactor
    Signed-off-by: Sumit <kumarxumit6@gmail.com>

--- arrays/practice/qus18.java
+++ arrays/practice/qus18.java
@@ -4,14 +4,22 @@ import java.util.Scanner;
 public class qus18 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<>();
 
-        ArrayList<Integer> list = new ArrayList<>(6);
-        // list.add(sc.nextInt());
-        
-        for (int i = 0; i < 6; i++) {
+        System.out.println("Enter integers:");
+
+        while (sc.hasNextInt()) {
             list.add(sc.nextInt());
         }
         System.out.println(list);
 
+        int maxElement = Integer.MIN_VALUE;
+        for (int num : list) {
+            if (num > maxElement) {
+                maxElement = num;
+            }
+        }
+
+        System.out.println("Maximum element: " + maxElement);
     }
 }
